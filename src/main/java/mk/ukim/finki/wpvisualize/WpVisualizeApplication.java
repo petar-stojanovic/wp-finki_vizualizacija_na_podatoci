db95013bd3b8df9a1e2ab9ba6162bd48c3b8c6db
petar-stojanovic <2001petarstojanovic@gmail.com>
2023-04-15 19:42:46 +0200

    created Spring Boot App

---
+++ src/main/java/mk/ukim/finki/wpvisualize/WpVisualizeApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wpvisualize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpVisualizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpVisualizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/wpvisualize/WpVisualizeApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wpvisualize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpVisualizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
